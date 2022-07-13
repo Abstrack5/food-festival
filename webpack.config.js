c5bfacc44232b8511fb845b76693484054c2c629
Abstrack <Armon_ah05@outlook.com>
2022-07-12 22:09:06 -0400

    optimized images, finished 19.3

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,39 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
   },
+  module: {
+    rules: [
+      {
+        // This object will identify the type of files to pre-process 
+        // using the test property to find a regular expression, or regex. 
+        // In our case, we are trying to process any image file with the file 
+        // extension of .jpg. We could expand this expression to also search for 
+        // other image file extensions such as .png, .svg, or .gif.
+        test: /\.jpg$/i,
+        // where the actual loader is implemented
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              // returns the name of the file with the file extension.
+              esModule: false,
+              name(file) {
+                return "[path][name].[ext]"
+              },
+              // a function that changes our assignment URL by replacing the ../ from 
+              // our require() statement with /assets/
+              publicPath: function(url) {
+                return url.replace("../", "/assets/")
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
+  },
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
